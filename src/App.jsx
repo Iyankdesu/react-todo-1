4924f60fa3ffb7f6283e843b26264b0f7a684ab8
Muhammad Fajrian Nur Ibrahim <iyankdesu@gmail.com>
2024-06-04 22:56:37 +0800

    add_css_to_components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
-import React, { useState, createContext } from "react";
+import React, { useState } from "react";
 import Todos from "./components/Todos";
-import TodoForm from "./components/TodoForm";
-
-export const TodoContext = createContext();
 
 function App() {
 	const [todos, setTodos] = useState([
@@ -23,42 +20,13 @@ function App() {
 		},
 	]);
 
-	const toggleCompleted = todoId => {
-		const updatedTodos = todos.map(todo => {
-			if (todo.id === todoId) {
-				todo.completed = !todo.completed;
-			}
-			return todo;
-		});
-		setTodos(updatedTodos);
-	};
-
-	const deleteTodo = todoId => {
-		const updatedTodos = todos.filter(todo => todo.id !== todoId);
-		setTodos(updatedTodos);
-	};
-
-	const addTodo = todoTitle => {
-		if (todoTitle === "") {
-			return;
-		}
-		const newTodo = {
-			id: todos.length + 1,
-			title: todoTitle,
-			completed: false,
-		};
-		const updatedTodos = todos.concat(newTodo);
-		setTodos(updatedTodos);
-	};
+	console.log(todos);
 
 	return (
-		<TodoContext.Provider value={{ toggleCompleted, deleteTodo }}>
-			<div style={styles.container}>
-				<h1 style={styles.title}>My Todo List</h1>
-				<TodoForm addTodo={addTodo} />
-				<Todos todos={todos} />
-			</div>
-		</TodoContext.Provider>
+		<div style={styles.container}>
+			<h1 style={styles.title}>My Todo List</h1>
+			<Todos todos={todos} />
+		</div>
 	);
 }
 
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,55 +1,16 @@
-import React, { useContext } from "react";
-import { TodoContext } from "../App";
+import React from "react";
 
 const TodoItem = ({ todo }) => {
-	const { toggleCompleted, deleteTodo } = useContext(TodoContext);
-
-	const getTodoTitleStyle = () => {
-		if (todo.completed === true) {
-			return { textDecoration: "line-through" };
-		} else {
-			return { textDecoration: "none" };
-		}
-	};
-
 	return (
-		<div style={styles.todoItem}>
-			<input
-				type="checkbox"
-				style={styles.checkbox}
-				onChange={() => toggleCompleted(todo.id)}
-			/>
-			<p style={getTodoTitleStyle()}>{todo.title}</p>
-			<button style={styles.button} onClick={() => deleteTodo(todo.id)}>
-				x
-			</button>
+		<div style={style}>
+			<p>{todo.title}</p>
 		</div>
 	);
 };
 
-const styles = {
-	todoItem: {
-		border: "2px solid #f4f4f4",
-		fontSize: "24px",
-		display: "flex",
-		justifyContent: "space-between",
-		alignItems: "center",
-		padding: "0 20px",
-	},
-	checkbox: {
-		height: "18px",
-		width: "18px",
-	},
-	button: {
-		backgroundColor: "#BB0000",
-		color: "#fff",
-		height: "30px",
-		width: "30px",
-		borderRadius: "100%",
-		border: "none",
-		cursor: "pointer",
-		fontSize: "16px",
-	},
+const style = {
+	border: "2px solid #f4f4f4",
+	fontSize: "24px",
 };
 
 export default TodoItem;
